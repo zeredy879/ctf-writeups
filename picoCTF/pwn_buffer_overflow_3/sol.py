6b29be3cd0db25833b2d48f6cad60fe5df669035
zeredy879 <903796007@qq.com>
2022-10-27 20:19:59 +0800

    a little update

--- picoCTF/pwn_buffer_overflow_3/sol.py
+++ picoCTF/pwn_buffer_overflow_3/sol.py
@@ -19,9 +19,9 @@ for i in range(4):
         r.close()
 success("canary: " + canary.decode())
 
-# This challenge is a little disgusting, my connection to remote is too slow, 
+# This challenge is a little disgusting, my connection to remote is too slow,
 # so I use picoCTF's Webshell. But after brute-force the canary I can't get flag
-# dump on the Webshell, therefore I use the same canary at local and it works. 
+# dump on the Webshell, therefore I use the same canary at local and it works.
 # Then I found the canaries are the same on all the container, you can use any others'
 # canary, it's all "BiRd". picoCTF should really improve there Webshell, it makes
 # me confused that Webshell need to re-login after 10-30 seconds.
